d77e83c2c2a5afc7cc0ee35c0a7cbf80416158fd
naabvb <46524999+naabvb@users.noreply.github.com>
2019-12-01 03:17:18 +0200

    Add missing path generalizations

--- removeEmailsAndFiles.py
+++ removeEmailsAndFiles.py
@@ -19,8 +19,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(BASE_PATH + 'token.pickle'):
+        with open(BASE_PATH + 'token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -28,10 +28,10 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                BASE_PATH + 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(BASE_PATH + 'token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     service = build('gmail', 'v1', credentials=creds)
 
